31483369512f55b2a2573f2d1e5a0f7d016d3d75
Bao Nguyen Hoai <nhbao.it@gmail.com>
2025-12-26 23:58:59 +0700

    BTS-6 Create and Migrate DbContext
    BTS-6 Create and Migrate DbContext

---
+++ src/CMSBlog.Data/CMSBlogDBContextFactory.cs.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace CMSBlog.Data
+{
+    public class CMSBlogDBContextFactory : IDesignTimeDbContextFactory<CMSBlogDBContext>
+    {
+        public CMSBlogDBContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var optionsBuilder = new DbContextOptionsBuilder<CMSBlogDBContext>();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            return new CMSBlogDBContext(optionsBuilder.Options);
+        }
+    }
+}
