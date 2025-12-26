41da98a19760ee86631abd5209169caec75e956c
Bao Nguyen Hoai <nhbao.it@gmail.com>
2025-12-26 22:46:40 +0700

    BTS-5 Intergrate Identity into Core
    BTS-5 Intergrate Identity into Core

---
+++ src/CMSBlog.Core/Domain/Identity/AppUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CMSBlog.Core.Domain.Identity
+{
+    [Table("AppUsers")]
+    public class AppUser : IdentityUser<Guid>
+    {
+        [Required]
+        [MaxLength(256)]
+        public required string FisrtName { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public required string LastName { get; set; }
+
+        public bool IsActive { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? Dob { get; set; }
+
+        [MaxLength(512)]
+        public string? Avatar { get; set; }
+        public DateTime? VipStartDate { get; set; }
+        public DateTime? VipExprieDate { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+        public double Balance { get; set; }
+
+    }
+}
